62c2a9dce662e6e6e2cd86f53d63b151952f277f
Sercan <sercansimsek44@gmail.com>
2026-07-23 10:03:31 +0200

    Week 1 Workshop - Task 1

--- server.js
+++ server.js
@@ -10,28 +10,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
-
-app.get("/campsites/:campsiteId", (req, res) => {
-  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post("/campsites/:campsiteId", (req, res) => {
-  res.statusCode = 403;
-  res.end(
-    `POST operation is not supported on /campsites/${req.params.campsiteId}`,
-  );
-});
-
-app.put("/campsites/:campsiteId", (req, res) => {
-  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-  res.end(
-    `Will update the capsite: ${req.body.name} with description: ${req.body.description}`,
-  );
-});
-
-app.delete("/campsites/:campsiteId", (req, res) => {
-  res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+app.use("/:campsiteId", campsiteRouter);
 
 app.use(express.static(__dirname + "/public"));
 
