98378f6352c5f7133134c143915cf994db61de83
Sercan <sercansimsek44@gmail.com>
2026-07-24 09:17:30 +0200

    Cleanup according to the feedback - Week 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const campsiteRouter = express.Router();
 
+/*  Consider setting status codes explicitly within each handler rather than defaulting to 200 in .all(). It works as you override later, but it's clearer to set headers in .all() and set status codes in the specific handlers. For example, use res.status(201) for successful POST creation, 200 for standard success, 204 for delete with no body, and 403 for unsupported operations. */
+
 campsiteRouter
   .route("/")
   .all((req, res, next) => {
@@ -12,6 +14,7 @@ campsiteRouter
     res.end("Will send all the campsites to you");
   })
   .post((req, res) => {
+    res.statusCode = 201;
     res.end(
       `Will add the campsite: ${req.body.name} with description: ${req.body.description}`,
     );
@@ -21,6 +24,7 @@ campsiteRouter
     res.end("PUT operation not supported on /campsites");
   })
   .delete((req, res) => {
+    res.statusCode = 204;
     res.end("Deleting all campsites");
   });
 
@@ -45,7 +49,7 @@ campsiteRouter
   .put((req, res) => {
     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
     res.end(
-      `Will update the capsite: ${req.body.name} with description: ${req.body.description}`,
+      `Will update the campsite: ${req.body.name} with description: ${req.body.description}`,
     );
   })
   .delete((req, res) => {
